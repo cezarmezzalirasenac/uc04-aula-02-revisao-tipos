341c9e4778b2ca14660af8d2a6b3abb42cc96624
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-11-13 21:31:17 -0300

    Revisão palavras reservadas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,69 @@
+// Isso é um comentário
+/**
+ * Isso também é um comentário
+ */
+
+// tipos de dados
+//number ou numérico (Inteiros)
+let valor: number = 1;
+
+//boolean ou booleano (falso ou verdadeiro)
+let valorBooleano: boolean = false;
+
+// string ou cadeia de caracteres
+let valorTexto: string = "Olá pessoal!";
+
+// tipo não definido
+let valorIndefinido = undefined;
+
+// tipo nulo
+let valorNulo = null;
+
+const outroValor = 0;
+
+console.log("Mensagem no terminal");
+
 console.log("Olá mundo");
+
+// palavras reservadas
+
+if (1) {
+} else {
+  console.log("");
+}
+
+while (0) {}
+
+do {} while (0);
+{
+}
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+const chave = 10;
+
+switch (chave) {
+  case 10:
+    console.log("Chave 10");
+    break;
+  default:
+    console.log("Chave inválida");
+    break;
+}
+
+function teste(valor: string) {
+  return "";
+}
+
+// let novoValor: number;
+let novoValor = 5 + 3;
+// novoValor = "5";
+novoValor = 5;
+console.log();
+
+// number
+// Inteiros
+// Double, Float, Números com casas decimais
+//
